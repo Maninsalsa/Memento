fe3333ada15b73b89e5dd37be8b8b06531d9373b
maninsalsa <robert.manansala@gmail.com>
2025-03-25 11:10:08 -0700

    gitignore update

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-# # src/__init__.py
-
-# # Importing key modules to expose them at the package level
-# from .main import main
-# from .config import *  # Import all constants and settings
-# from .game_state import GameState  # Example of importing a specific class
-
-# # Optionally, you can define the __all__ variable to control what is exported
-# __all__ = ['main', 'GameState']  # Specify public API
